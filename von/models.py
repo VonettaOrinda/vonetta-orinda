07c27d6333dc7f4d706473a819bebe8f70729bb6
VONETTA <thevonetteway@gmail.com>
2022-08-03 22:52:26 +0300

    added models

--- von/models.py
+++ von/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class About(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Contacts(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    
+class Resume(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    
+class Projects(models.Model):
+    name= models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    
+    @classmethod
+    def all_projects(self):
+
+        return Projects.objects.all()
+    
+    
+    
+
+
