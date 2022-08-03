63e97c79237a5789c1bf6e7d9457e66df5e3c3da
VONETTA <thevonetteway@gmail.com>
2022-08-03 22:54:14 +0300

    added views

--- von/views.py
+++ von/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from.models import*
+from.models import about,contact
 # Create your views here.
+
+def home(request):
+    about=About.objects.all()
+    projects=Projects.objects.all()
+    contact=Contacts.objects.all()
+    resume=Resume.objects.all()
+    
+    
+    return render (request,"all-von/von.html",{"about":about,"projects":projects,"contact":contact,"resume":resume} )
+    
+    
