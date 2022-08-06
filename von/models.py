ffa6fd94d893e22366e854262e378393f76bf04f
VONETTA <thevonetteway@gmail.com>
2022-08-06 12:56:27 +0300

    added image to models

--- von/models.py
+++ von/models.py
@@ -29,6 +29,17 @@ class Projects(models.Model):
 
         return Projects.objects.all()
     
+class Image(models.Model):
+    name= models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    gallery_image = models.ImageField(upload_to='uploads/')
+
+
+    @classmethod
+    def all_images(self):
+
+        return Image.objects.all()
+    
     
     
 
