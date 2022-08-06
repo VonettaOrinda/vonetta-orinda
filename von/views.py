54695e32ba809c9b3786aa52a2381bb242f7110e
VONETTA <thevonetteway@gmail.com>
2022-08-06 13:03:09 +0300

    added image to views

--- von/views.py
+++ von/views.py
@@ -1,7 +1,8 @@
 from email.mime import image
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from.models import*
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -14,5 +15,12 @@ def home(request):
     
     
     return render (request,"all-von/von.html",{"about":about,"projects":projects,"contact":contact,"resume":resume,"image":image} )
+
+def get_image_by_id(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request,"image.html", {"image":image})
     
     
